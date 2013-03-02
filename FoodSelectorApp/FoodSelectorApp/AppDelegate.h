680b89787881debaf82ef2cb62d9cca6e3411e57
daviad <592597406@qq.com>
2013-03-02 20:22:50 +0800

    commit workspace

---
+++ FoodSelectorApp/FoodSelectorApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FoodSelectorApp
+//
+//  Created by dxw on 13-3-2.
+//  Copyright (c) 2013年 daviad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
